7282df920be25511118cc43e963b1e7332369c83
JanKoch99 <jankoch99@bluewin.ch>
2021-10-07 21:36:12 +0200

    update user.service.ts
    imported checks for password

--- ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/user.service.ts
@@ -2,11 +2,18 @@ import { UserAttributes, User } from '../models/user.model';
 import { LoginResponse, LoginRequest } from '../models/login.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import {ifError} from "assert";
 
 export class UserService {
 
     public register(user: UserAttributes): Promise<UserAttributes> {
         const saltRounds = 12;
+        switch (this.passwordCheck(user.password)) {
+            case 1 : return Promise.reject({message: 'Doesnt contain capital/small letter'});
+            case 2 : return Promise.reject({message: 'Doesnt contain number'});
+            case 3 : return Promise.reject({message: 'Doesnt contain special character'});
+            case 4 : return Promise.reject({message: 'Minimum of 8 characters'});
+        }
         user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
         return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
     }
@@ -34,5 +41,22 @@ export class UserService {
 
     public getAll(): Promise<User[]> {
         return User.findAll();
+    }
+
+    private passwordCheck(password: string): number {
+        const capital_small = new RegExp('^(?=.*[a-z])(?=.*[A-Z])+$');
+        const number = new RegExp('^(?=.*\\d)+$');
+        const specialCharacter = new RegExp('^(?=.*[-+_!@#$%^&*.,?])+$');
+        const minimum = new RegExp('^(.{8,25})+$');
+        if (capital_small.test(password)) {
+            return 1;
+        } else if (number.test(password)) {
+            return 2;
+        } else if (specialCharacter.test(password)) {
+            return 3;
+        } else if (minimum.test(password)) {
+            return 4;
+        }
+        return 0;
     }
 }
