427b3fadd57d35acc8120fbc5fa4c617fc354083
Kevin Stöckli <82031534+ShutCoffee@users.noreply.github.com>
2021-10-23 19:33:18 +0200

    Creation of Post Model

---
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/models/post.model.ts
@@ -0,0 +1,13 @@
+export class Post {
+
+  constructor(
+    public title: string,
+    public text: string,
+    public image: Blob,
+    public category: string,
+    public upvotes: number,
+    public downvotes: number,
+    public created_at: String,
+    public userId: string
+  ) {}
+}
--- ese2021-project-scaffolding/scaffolding/frontend/src/app/post/post.component.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/post/post.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  post: Post = new Post('', '', new Blob(), '', 0, 0, '', '')
+
+  @Output()
+  update = new EventEmitter<Post>();
+
+  @Output()
+  delete = new EventEmitter<Post>();
+
+  constructor(
+    public httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
   }
 
+  updatePost(): void {
+    this.update.emit(this.post);
+  }
+
+  deletePost(): void {
+    this.delete.emit(this.post);
+  }
 }
