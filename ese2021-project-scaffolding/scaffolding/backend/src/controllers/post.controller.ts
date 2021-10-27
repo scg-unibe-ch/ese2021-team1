8d30726d6f54b39b196bf1c0cf76f4c049e2a97b
aris.konstantinidis <aris.konstantinidis@students.unibe.ch>
2021-10-27 17:40:35 +0300

    update comments in back and front

--- ese2021-project-scaffolding/scaffolding/backend/src/controllers/post.controller.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/controllers/post.controller.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
-import { Post } from '../models/post.model';
 import { PostService } from '../services/post.service';
-import {MulterRequest} from '../models/multerRequest.model';
 
 const postController: Router = express.Router();
 const postService = new PostService();
 
 postController.post('/', (req: Request, res: Response) => {
     console.log(req.body); // this object contains the new post that the frontend sent us
-    // then we process to pass this post object to the appropriate service that we also have to create
+    // we pass this post object to the appropriate service that we also have to create
     postService.createPost(req.body)
-        // we await the creation of the new post and send it back if everything went ok
+        // we await the creation of the new post and send it back to the front if everything went ok
         .then(post => res.json(post))
         // else if something went wrong we catch it and send the appropriate feedback to the front
-        .catch(err => res.json(err.message));
+        .catch(err => res.json(err));
     //     .then(inserted => res.send(inserted))
     //     .catch(err => res.status(500).send(err));
 });
--- ese2021-project-scaffolding/scaffolding/backend/src/services/post.service.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/services/post.service.ts
@@ -5,17 +5,19 @@ import {MulterRequest} from '../models/multerRequest.model';
 import {Post} from '../models/post.model';
 
 export class PostService {
-    // this function (aka service) is responsible for converting the received object (parameter)
+
+    // @jan, @alessio
+
+    // this function (aka service) is responsible for converting the received object (see method parameter)
     // into the right Post format and store it in the database. It also has to give some
     // feedback back to the controller which then will send it to the front
     public async createPost(post: { title: string, content: string, labels: string[], userName: string }) {
-        // in the parameter signature we can define and "type" the parameters that we get
-        // whatever we do that is asynchronous, we can denote that with async
-        return Post.create({ // we use the model's inherited methods (here create) to store the new post in the db -
-            // prior to that we have to "convert" it to the correct format
-            title: post.title,
+        // in the parameter signature we can define and "type" the parameters that we get, for now I just made it as simple as possible
+        return Post.create({ // we use the model's inherited methods (like create) to store the new post in the db
+            // prior to that we have to "create" a valid post with the data we took from the front
+            title: post.title, // these attributes come from the object that the front sent us
             text: post.content,
-            image: null,
+            image: null, // default for now...
             downvotes: 0,
             upvotes: 0,
             category: '',
@@ -23,13 +25,93 @@ export class PostService {
         })
         // now we want to check whether the creation was successful
         .then(inserted => {
-            // return the inserted row (Post) to the controller
+            // rif all ok, return the inserted row (Post) to the controller
             return Promise.resolve(inserted);
         })
+        // else if an error occured
         .catch(err => {
             // return the error message
             return Promise.reject(err.message);
         });
     }
+
+            // public addImage(req: MulterRequest): Promise<ItemImageAttributes> {
+        //     return TodoItem.findByPk(req.params.id)
+        //         .then(found => {
+        //             if (!found) {
+        //                 return Promise.reject('Product not found!');
+        //             } else {
+        //                 return new Promise<ItemImageAttributes>((resolve, reject) => {
+        //                     upload.single('image')(req, null, (error: any) => {
+        //                         ItemImage.create({ fileName: req.file.filename, todoItemId: found.todoItemId })
+        //                             .then(created => resolve(created))
+        //                             .catch(() => reject('Could not upload image!'));
+        //                     });
+        //                 });
+        //             }
+        //         })
+        //         .catch(() => Promise.reject('Could not upload image!'));
+        // }
+
+
+        // public getImageItem(imageId: number): Promise<ItemImage> {
+        //     return ItemImage.findByPk(imageId)
+        //         .then(image => {
+        //             if (image) {
+        //                 return Promise.resolve(image);
+        //             } else {
+        //                 return Promise.reject('image not found!');
+        //             }
+        //         })
+        //         .catch(() => Promise.reject('could not fetch the image!'));
+        // }
+
+
+        // public getAllPosts(): Promise<Post[]> {
+        //     return Post.findAll()
+        //         .then(post => {
+        //             if (post) {
+        //                 return Promise.resolve(posts); // TODO: is it post or the table posts from post.model.ts that we should return?
+        //             } else {
+        //                 return Promise.reject('posts not found');
+        //             }
+        //         })
+        //         .catch(() => Promise.reject('could not fetch the posts'));
+        // }
+
+        // // TODO: when createPost is called, call addImage to add the image too (or find a way to add it in here directly)
+        // public createPost(title, text, category, userId): Promise<Post> {
+        //     return Post.create().then(post => {
+        //         if (title != null) {
+        //             if (category != null) {
+        //                 if (userId != null) {
+        //                     post.title = title;
+        //                     post.text = text;
+        //                     post.category = category;
+        //                     post.userId = userId;
+        //                     post.created_at = Date.now();
+        //                     return Promise.resolve(post);
+        //                 } else {
+        //                     return Promise.reject('userID of the user is missing');
+        //                 }
+        //             } else {
+        //                 return Promise.reject('category is missing');
+        //             }
+        //         } else {
+        //             return Promise.reject('post title is missing');
+        //         }
+        //     }).catch(() => Promise.reject('some fields may be empty'));
+        // }
+
+
+        // public updatePost(): Promise<Post[]> {
+        //     return null;
+        // }
+
+        // public deletePost(postID): Promise {
+        //     return Post.findByPk(postID)
+        //         .then()
+        //         .catch();
+        // }
 
 }
