ed0d07d052d8c3185fe414e27d910e80898f0d12
4l3s5i0Z <alessio.petrone@students.unibe.ch>
2021-11-09 14:53:42 +0100

    surprise :P

--- ese2021-project-scaffolding/scaffolding/backend/src/server.ts
+++ ese2021-project-scaffolding/scaffolding/backend/src/server.ts
@@ -75,7 +75,7 @@ export class Server {
             .options('*', cors(options))
             .use(express.static('./src/public'))
             // this is the message you get if you open http://localhost:3000/ when the server is running
-            .get('/', (req, res) => res.send('<h1>Welcome to the ESE-2021 Backend Scaffolding <span style="font-size:50px">&#127881;</span></h1>'));
+            .get('/', (req, res) => res.send('<h1>Welcome to Jan and Alessios domain <span style="font-size:50px">&#128525;</span></h1>'));
     }
 
     private configureSequelize(): Sequelize {
