f83187f35ee0d67811eca315ee0bae69a670120b
JanKoch99 <jankoch99@bluewin.ch>
2021-11-10 12:56:03 +0100

    created product.model.ts

---
+++ ese2021-project-scaffolding/scaffolding/backend/src/models/product.model.ts
@@ -0,0 +1,69 @@
+import { Model, Sequelize, DataTypes } from 'sequelize';
+
+export interface ProductProperties {
+    id: number; // used for the database
+    title: string;
+    image: Blob;
+    description: string;
+    category: string; // separated by a comma
+    available: boolean;
+    price: number;
+    discount: number;
+}
+
+
+
+export class Product extends Model<ProductProperties> implements ProductProperties {
+    id: number;
+    title: string;
+    image: Blob;
+    description: string;
+    category: string;
+    available: boolean;
+    price: number;
+    discount: number;
+
+    public static initialize(sequelize: Sequelize) {
+        Product.init({
+                id: {
+                    type: DataTypes.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    defaultValue: 0
+                },
+                title: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+
+                image: {
+                    type: DataTypes.BLOB,
+                    allowNull: false
+                },
+                description: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                category: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+            available: {
+                    type: DataTypes.BOOLEAN,
+                    allowNull: false
+            },
+            price: {
+                    type: DataTypes.FLOAT,
+                    allowNull: false
+            },
+            discount: {
+                    type: DataTypes.FLOAT,
+                    defaultValue: 1
+            }
+            },
+            {
+                sequelize,
+                tableName: 'products'
+            }
+        ); }
+}
