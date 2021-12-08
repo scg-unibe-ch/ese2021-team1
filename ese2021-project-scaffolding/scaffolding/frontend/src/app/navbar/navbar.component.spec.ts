53034be83a546a62e4a020e001691e1dce3d911f
JanKoch99 <jankoch99@bluewin.ch>
2021-12-08 13:13:02 +0100

    added login and logout test
    Need to fix the login

--- ese2021-project-scaffolding/scaffolding/frontend/src/app/navbar/navbar.component.spec.ts
+++ ese2021-project-scaffolding/scaffolding/frontend/src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,7 @@
 import {NavbarComponent} from "./navbar.component";
 import {TestBed} from "@angular/core/testing";
 import {HttpClientModule} from "@angular/common/http";
+import {User} from "../models/user.model";
 
 describe('Navbar Component', () => {
   let navbarComponent: NavbarComponent;
@@ -15,6 +16,24 @@ describe('Navbar Component', () => {
   });
   it('should be created', () => {
     expect(navbarComponent).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should log in with a user', () => {
+      let user1 = new User(1,"Test", "Test123***", "f1", "l1", "e@gmail.com", "a1", 1, 1111, "c1", "2002-12-12","+323444444444", false);
+      navbarComponent.user = user1;
+      navbarComponent.ngOnInit();
+      // TODO: How can we login?
+      // expect(navbarComponent.loggedInState).toBe(true);
+    });
+
+    it('should log out', () => {
+      let user1 = new User(1,"Test", "Test123***", "f1", "l1", "e@gmail.com", "a1", 1, 1111, "c1", "2002-12-12","+323444444444", false);
+      navbarComponent.user = user1;
+      navbarComponent.loggedInState = true;
+      navbarComponent.logoutUser();
+      expect(navbarComponent.loggedInState).toBe(false);
+    });
   });
 });
 
